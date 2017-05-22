6e369194736fb25eb3ac6dcc51c8287cc8852430
Masaya Hayashi <mcdonalds.only@gmail.com>
2017-05-23 03:04:41 +0900

    Show repositories on table view

--- github-repo-search/SearchRepoVC.swift
+++ github-repo-search/SearchRepoVC.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class SearchRepoVC: UIViewController {
 
+    // - Properties
+
+    fileprivate var repositories: [Repository] = [] {
+        didSet {
+            self.repoTableView.reloadData()
+        }
+    }
+
     // - User Interface
 
     @IBOutlet private weak var searchBar: UISearchBar!
@@ -22,3 +30,21 @@ class SearchRepoVC: UIViewController {
     }
 }
 
+extension SearchRepoVC: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.repositories.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        cell.textLabel?.text = repositories[indexPath.row].fullName
+        if let language = repositories[indexPath.row].language {
+            cell.detailTextLabel?.text = language
+        }
+        return cell
+    }
+}
