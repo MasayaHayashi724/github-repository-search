43b75a03562b4ca8f0dae517ece928b2c5315b87
Masaya Hayashi <mcdonalds.only@gmail.com>
2017-05-23 01:43:03 +0900

    Setup VC for showing repo page

---
+++ github-repo-search/RepoPageVC.swift
@@ -0,0 +1,22 @@
+//
+//  RepoPageVC.swift
+//  github-repo-search
+//
+//  Created by Masaya Hayashi on 2017/05/23.
+//  Copyright © 2017年 Masaya Hayashi. All rights reserved.
+//
+
+import UIKit
+
+class RepoPageVC: UIViewController {
+
+    // - User Interface
+
+    @IBOutlet private weak var repoPageWebView: UIWebView!
+
+    // - View Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
