4741361743dd8dceaf644041ca11232f5a4810db
Masaya Hayashi <mcdonalds.only@gmail.com>
2017-05-23 01:03:01 +0900

    Setup project

---
+++ github-repo-search/SearchRepoVC.swift
@@ -0,0 +1,17 @@
+//
+//  SearchRepoVC.swift
+//  github-repo-search
+//
+//  Created by Masaya Hayashi on 2017/05/23.
+//  Copyright © 2017年 Masaya Hayashi. All rights reserved.
+//
+
+import UIKit
+
+class SearchRepoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
